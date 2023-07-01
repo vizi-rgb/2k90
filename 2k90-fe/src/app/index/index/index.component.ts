cb85d8cfab96b2ea4031db2d1fcac3722e7108b6
vizi <wiktor.nasierowski@gmail.com>
2023-07-01 13:15:08 +0200

    WEB-4 Created index view

--- 2k90-fe/src/app/index/index/index.component.ts
+++ 2k90-fe/src/app/index/index/index.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class IndexComponent {
 
+  quote: String = "\"I hate being bipolar, it's awesome!\""
+
+  onIndexLogoClick() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }

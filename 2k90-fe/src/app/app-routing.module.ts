ad0ca3d34217b8af444bfece10b70dbc6469577b
vizi <wiktor.nasierowski@gmail.com>
2023-07-17 12:56:08 +0200

    WEB-40 Added pageNotFound view

--- 2k90-fe/src/app/app-routing.module.ts
+++ 2k90-fe/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './login/login/login.component';
 import { RegisterComponent } from './register/register/register.component';
 import { IndexComponent } from './index/index/index.component';
 import { RegisterSuccessComponent } from './register/register-success/register-success.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'register/success', component: RegisterSuccessComponent },
-  { path: '', component: IndexComponent },
+  { path: '', component: IndexComponent }, 
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({

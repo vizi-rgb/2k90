9fef5cdd97320047f06e59eff9b735afdc1b08e9
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-10-02 00:24:13 +0200

    Added login, logout (with bugs)

--- 2k90-fe/src/app/auth-guards.guard.ts
+++ 2k90-fe/src/app/auth-guards.guard.ts
@@ -1,24 +1,26 @@
 import { inject } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot, CanActivateFn, ROUTER_CONFIGURATION, Router, RouterStateSnapshot, createUrlTreeFromSnapshot } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
 
 export const notLoggedInGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
+  state: RouterStateSnapshot,
 ) => {
-  const isAnonymous: boolean = true;
+  const isAnonymous: boolean = !inject(AuthenticationService).isAuthenticated;
+  console.log(isAnonymous);
 
   if (isAnonymous) {
     return inject(Router).createUrlTree(["/", "login"]);
   }
 
-  return false;
+  return true;
 };
 
 export const landingPageGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  const isAnonymous: boolean = true;
+  const isAnonymous: boolean = !inject(AuthenticationService).isAuthenticated;
 
   return isAnonymous ?
     true :
--- 2k90-fe/src/app/services/authentication.service.ts
+++ 2k90-fe/src/app/services/authentication.service.ts
@@ -1,17 +1,72 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { LoginUserDTO } from '../interfaces/login-user-interface';
+import { User } from '../interfaces/user';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
+  readonly loginEndpoint: string;
+
+  private authenticated: boolean;
+  private basicAuthorization: HttpHeaders | undefined;
+  private userSubject: BehaviorSubject<User | null>;
+  public user: Observable<User | null>;
 
   constructor(
     private router: Router,
     private http: HttpClient
   ) {
+    this.authenticated = false;
+    this.loginEndpoint = "http://localhost:8080/api/auth/login";
+
+    this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
+    this.user = this.userSubject.asObservable();
+    if (this.userSubject.value) {
+      this.authenticated = true;
+    }
+  }
+
+  get isAuthenticated(): boolean {
+    return this.authenticated;
+  }
+
+  public get userValue() {
+    return this.userSubject.value;
+  }
+
+  doLogin(userDTO: LoginUserDTO): void {
+    this.http.post(this.loginEndpoint, userDTO, {responseType: 'text'})
+      .subscribe({
+
+        next: (v) => {
+          this.authenticated = true;
+          const base64UsernamePassword: string = btoa(userDTO.username + ':' + userDTO.password);
+          this.basicAuthorization = new HttpHeaders({'Authorization:': 'Basic' + base64UsernamePassword});
+          localStorage.setItem('user', JSON.stringify(v));
+          const vJson = JSON.parse(v);
+          this.userSubject.next({
+            id: vJson.id,
+            username: vJson.username,
+            email: vJson.email
+          });
+        },
+
+        error: (e) => {
+            throw new Error("Wrong credentials");
+        },
+
+        complete: () => console.log('complete')
+      });
+  }
+
+  doLogout() {
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['/login']);
   }
 }

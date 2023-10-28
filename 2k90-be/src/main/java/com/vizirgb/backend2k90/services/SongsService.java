515e2a27a99442742e7c30f63f61dda39187ef46
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-10-28 20:50:16 +0200

    Added method for GET song by id

--- 2k90-be/src/main/java/com/vizirgb/backend2k90/services/SongsService.java
+++ 2k90-be/src/main/java/com/vizirgb/backend2k90/services/SongsService.java
@@ -1,6 +1,10 @@
 package com.vizirgb.backend2k90.services;
 
+import com.vizirgb.backend2k90.entities.AlbumEntity;
+import com.vizirgb.backend2k90.entities.ArtistEntity;
 import com.vizirgb.backend2k90.entities.SongEntity;
+import com.vizirgb.backend2k90.entities.SongPerformerEntity;
+import com.vizirgb.backend2k90.repositories.AlbumRepository;
 import com.vizirgb.backend2k90.repositories.ArtistRepository;
 import com.vizirgb.backend2k90.repositories.SongPerformerRepository;
 import com.vizirgb.backend2k90.repositories.SongRepository;
@@ -9,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Optional;
 
 @Service
@@ -18,6 +23,7 @@ public class SongsService {
     private final SongRepository songRepository;
     private final SongPerformerRepository songPerformerRepository;
     private final ArtistRepository artistRepository;
+    private final AlbumRepository albumRepository;
 
     public Optional<SongDTO> getSongById(long id) {
         Optional<SongEntity> optionalSongEntity = songRepository.findById(id);
@@ -25,12 +31,58 @@ public class SongsService {
             return Optional.empty();
         }
 
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+        LinkedList<String> artists = null;
+        try {
+            artists = getArtistsOnSong(id);
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
+        }
+
+        SongEntity songEntity = optionalSongEntity.get();
+        String albumName = getAlbumName(songEntity.getAlbumId());
+        SongDTO song = SongDTO.builder()
+                .id(songEntity.getId())
+                .duration(songEntity.getDuration())
+                .title(songEntity.getTitle())
+                .albumName(albumName)
+                .authors(artists)
+                .build();
+
+        return Optional.of(song);
     }
 
-    private Collection<String> getArtistsOnSong(long id) {
-        // TODO
-        throw new UnsupportedOperationException("TODO");
+    private LinkedList<String> getArtistsOnSong(long songId) {
+        Collection<SongPerformerEntity> songPerformerEntities = songPerformerRepository.findAllBySongId(songId);
+        if (songPerformerEntities.isEmpty()) {
+            throw new IllegalArgumentException("There are not any artists on the song with the provided id");
+        }
+
+        long mainArtistId = -1;
+        Collection<Long> songPerformerEntitiesIds = new LinkedList<>();
+        for (SongPerformerEntity entity : songPerformerEntities) {
+            songPerformerEntitiesIds.add(entity.getArtistId());
+            if (entity.isMainArtist()) {
+                mainArtistId = entity.getArtistId();
+            }
+        }
+
+        Iterable<ArtistEntity> artistEntities = artistRepository.findAllById(songPerformerEntitiesIds);
+
+        LinkedList<String> artists = new LinkedList<>();
+        for (ArtistEntity artistEntity : artistEntities) {
+            if (artistEntity.getId() == mainArtistId) {
+                artists.addFirst(artistEntity.getName());
+            } else {
+                artists.add(artistEntity.getName());
+            }
+        }
+
+        return artists;
+    }
+
+    private String getAlbumName(long albumId) {
+        Optional<AlbumEntity> optionalAlbumEntity = albumRepository.findById(albumId);
+
+        return optionalAlbumEntity.map(AlbumEntity::getTitle).orElse(null);
     }
 }

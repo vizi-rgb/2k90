6a0cfab637bd31756250406a5943b8a84e196e5e
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-10-28 10:59:13 +0200

    Changed UserDTO to include role attribute

--- 2k90-be/src/main/java/com/vizirgb/backend2k90/services/LoginService.java
+++ 2k90-be/src/main/java/com/vizirgb/backend2k90/services/LoginService.java
@@ -1,6 +1,9 @@
 package com.vizirgb.backend2k90.services;
 
+import com.vizirgb.backend2k90.dto.UserDTO;
 import com.vizirgb.backend2k90.dto.UserLoginDTO;
+import com.vizirgb.backend2k90.entities.UserEntity;
+import com.vizirgb.backend2k90.repositories.UserRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,11 +12,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
     public void login(@NotNull UserLoginDTO userLoginDTO) throws AuthenticationException {
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
@@ -23,5 +29,19 @@ public class LoginService {
                 );
 
         Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    public UserDTO onSuccessfulLogin(@NotNull UserLoginDTO userLoginDTO) {
+        Optional<UserEntity> userEntityOptional = userRepository
+                .findByUsername(userLoginDTO.getUsername());
+
+        UserDTO userDTO = userEntityOptional.map(userEntity ->
+                        new UserDTO(
+                            userEntity.getId(),
+                            userEntity.getUsername(),
+                            userEntity.getEmail(),
+                            userEntity.getRole()
+                        )).orElse(null);
+        return userDTO;
     }
 }

a8dc60adcb90f0a9bf3930f9dd216f72bcc3937e
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-10-28 18:27:25 +0200

    Added admin dashboard grid

--- 2k90-fe/src/app/admin-dashboard/admin-dashboard.component.ts
+++ 2k90-fe/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+  styleUrls: ['./admin-dashboard.component.css'],
 })
 export class AdminDashboardComponent {
-
+  albumCovers: string[] = ['astro.png', 'good-kid.png', 'views.png'];
 }
